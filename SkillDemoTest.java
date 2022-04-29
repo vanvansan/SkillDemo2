598f93e78ee8e940571d9d62baacd1f8b02148c0
vanvansan <103155715+vanvansan@users.noreply.github.com>
2022-04-28 21:42:53 -0700

    test updated 2

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(8, SkillDemo.test(2, 2));
+        assertEquals(8, SkillDemo.test(2, 4));
     }
 }
